744757128f0f92608e1b8479cf80f2b872302881
SP-Park <75590347+SP-Park@users.noreply.github.com>
2021-04-08 22:23:31 +0900

    Revert "Initial commit"
    This reverts commit 32a79a63b4efcdeaa042cb3a736fe3acf1541233.

--- client/src/components/Config.js
+++
@@ -1,6 +0,0 @@
-export const USER_SERVER = '/api/users';
-
-//API 엔드포인트 주소 중 공통 부분의 선언
-export const API_URL = 'https://api.themoviedb.org/3/';
-export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
-export const API_KEY = 'd032bb611a57d7b0a4045dca1e42e37e';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,58 +1,36 @@
-import React, { useEffect , useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
-import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../Config';
-import MainImage from './Sections/MainImage';
 
 function LandingPage(props) {
 
-   //state
-  const [Movies, setMovies] = useState([])
-  const [MainMovieImage, setMainMovieImage] = useState(null)
-
   useEffect(() => {
-    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-
-    fetch(endpoint)
-    .then(response => response.json()) //response를 바로 읽을 수는 없고 json을 통해 읽을 수 있음
-    .then(response => {
-      console.log(response)
-      setMovies([response.results]) //가져온 결과를 movie state에 넣어줌
-      setMainMovieImage(response.results[0]) //가장 인기있는 정보를 가져와서 메인에 넣어줌
-    })
-
+    axios.get('/api/hello')
+    .then(response => console.log(response.data))
   }, [])
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+    .then(response => {
+      console.log('Bye')
+      if(response.data.success) {
+        props.history.push("/login")
+      } else {
+        alert("Failed to sign out")
+      }
+    })
+  }
 
 
 
   return (
     <div style={{
-      width: '100%', margin: '0' }}>
-      
-      {/* Main Image */}
-      {MainMovieImage &&  // MainMovieImage 정보를 가져왔으면 아래 부분을 해라 라는 뜻
-      <MainImage 
-        image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`} 
-        title={MainMovieImage.original_title}
-        text={MainMovieImage.overview}
-      />
-      }
+      display: 'flex', justifyContent: 'center', alignItems: 'center'
+      , width: '100%', height: '100vh'
+    }}>
+      <h2>시작 페이지</h2>
 
-      <div style={{ width: '85%', margin: '1rem auto'}}>
-
-        <h2>Movies by latest</h2>
-        <hr />
-
-        {/* Movie Grid Cards */}
-
-
-      </div>
-
-      <div style={{ display: 'flex', justifyContent: 'center '}}>
-        <button>Load More</button>
-      </div>  
-    
-    
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   )
 }
