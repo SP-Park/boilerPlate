367b142e9f34374678788517295470449f990a0f
SP-Park <75590347+SP-Park@users.noreply.github.com>
2021-04-09 14:07:43 +0900

    MovieApp:Grid Cards, Load More

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,26 +3,37 @@ import axios from 'axios';
 import { withRouter } from 'react-router-dom';
 import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../../Config';
 import MainImage from './Sections/MainImage';
+import GridCards from '../commons/GridCards';
+import { Row } from 'antd';
 
 function LandingPage(props) {
 
   //state
   const [Movie, setMovie] = useState([])
   const [MainMovieImage, setMainMovieimage] = useState(null)
+  const [CurrentPage, setCurrentPage] = useState(0)
 
   useEffect(() => {
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetchMovies(endpoint);
 
+  }, [])
+
+  const fetchMovies = (endpoint) => {
     fetch(endpoint)
     .then(response => response.json())
     .then(response => {
       console.log(response)
-      setMovie([response.results])
+      setMovie([...Movie, ...response.results])   //기존에 있던 Movie state 에 새로운 response.results를 추가하도록 해줌 
       setMainMovieimage(response.results[0])
+      setCurrentPage(response.page)
     })
-  }, [])
+  } 
 
-
+  const loadMoreItems = () => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${ CurrentPage + 1 }`;
+    fetchMovies(endpoint);
+  }
 
 
 
@@ -42,10 +53,27 @@ function LandingPage(props) {
         <h2>Movies by latest</h2>
         <hr />
 
+        {/* Grid Cards 그리드 카드에 가져온 정보를 하나하나 넣어줌*/}  
+        <Row gutter={[ 16, 16]}>
+          
+          {Movie && Movie.map((movie, index) => (
+            <React.Fragment key={index}>
+              <GridCards 
+                image={movie.poster_path ?
+                `${IMAGE_BASE_URL}w500${movie.poster_path}` : null }
+                movieId={movie.id}   //무비 고유의 정보를 획득하기 위해 필요
+                movieName={movie.original_title}
+              />
+
+            </React.Fragment>
+          ))}
+        
+        </Row>
+
       </div>
 
       <div style={{ display: 'flex', justifyContent: 'center'}}>
-          <button>Load more </button>
+          <button onClick={loadMoreItems}>Load more</button>
       </div>
 
     </div>
